8d61b2c29fc6f99ce983b55a3ea1cfc2f8486580
Meshford <super.ya-nikitka-23@ya.ru>
2025-04-26 23:26:33 +0300

    4-th version

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// Обработка отправки формы с помощью Formspree
-document.getElementById('registration-form').addEventListener('submit', function(e) {
-    // Останавливаем стандартное поведение формы
+document.getElementById('registration-form').addEventListener('submit', async function(e) {
     e.preventDefault();
 
-    // Получаем данные из формы
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
     const phone = document.getElementById('phone').value;
 
-    // Проверка данных
-    if (firstName === '' || lastName === '' || phone === '') {
+    if (!firstName || !lastName || !phone) {
         alert("Пожалуйста, заполните все поля.");
         return;
     }
 
-    // Показать сообщение об успешной регистрации
     alert(`Спасибо за регистрацию, ${firstName}! Мы свяжемся с вами по телефону ${phone}.`);
 
-    // Отправка формы через Formspree
-    const form = document.getElementById('registration-form');
+    const formData = new FormData(this);
     
-    // Убедимся, что форма имеет правильный action
-    form.action = "https://formspree.io/f/mqaqaddz";  // Замените на ваш реальный URL из Formspree
-    form.method = "POST";  // Убедитесь, что метод POST указан
-    // Используем submit() для отправки данных формы
-    form.submit();
+    try {
+        const response = await fetch("https://formspree.io/f/mqaqaddz", {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            alert('Форма успешно отправлена!');
+        } else {
+            alert('Произошла ошибка при отправке формы.');
+        }
+    } catch (error) {
+        alert('Произошла ошибка.');
+    }
 });
