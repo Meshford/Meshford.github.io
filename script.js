b38d048886e4c30934965260bc9db9a07505529a
Meshford <super.ya-nikitka-23@ya.ru>
2025-04-26 22:29:07 +0300

    third version

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ document.getElementById('registration-form').addEventListener('submit', function
     const lastName = document.getElementById('last-name').value;
     const phone = document.getElementById('phone').value;
 
-    // Отправка формы на почту через AJAX
+    // Проверка данных
+    if (firstName === '' || lastName === '' || phone === '') {
+        alert("Пожалуйста, заполните все поля.");
+        return;
+    }
+
+    // Отправка формы на почту через mailto
     const emailContent = `Имя: ${firstName}\nФамилия: ${lastName}\nТелефон: ${phone}`;
     window.location.href = `mailto:super.ya-nikitka-23@ya.ru?subject=Заявка на курс Python&body=${encodeURIComponent(emailContent)}`;
 
